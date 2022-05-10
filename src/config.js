d5400e17ac96abf5f4970c9acfc3db7db450f5a6
DESKTOP-NDFI8ST\letam <tamlv.bhsoft@gmail.com>
2022-05-10 12:33:11 +0700

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   // url: 'https://online-courses-web.herokuapp.com'
-  url: 'http://localhost:5000'
+  url: window.location.origin.includes('localhost') ? 'http://localhost:5000' : 'https://online-courses-web.herokuapp.com',
 };
 
 export default config;
