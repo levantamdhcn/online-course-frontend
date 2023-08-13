3ea3a4a8e8c91974912390f2ed1e3846f67c0868
tamlv.bhsoft@gmail.com <tamlv.bhsoft@gmail.com>
2023-08-14 00:13:38 +0700

    url config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   // url: 'http://localhost:5000/api'
-  url: window.location.origin.includes('localhost') ? 'http://35.231.125.71/api' : 'https://api-elearning-portal.up.railway.app/api',
+  url: 'http://35.231.125.71/api',
 };
 
 export default config;
